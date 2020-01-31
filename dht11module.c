cb4e36f201d9c8b3fab130ec6739c0327907c205
mariamarkovska <markovska_maria@hotmail.com>
2020-01-31 13:31:15 +0100

    Added wiringPi and the real sensor code. Ready for testing on TinkerBoard

--- dht11module.c
+++ dht11module.c
@@ -6,7 +6,7 @@
 
 #include<python2.7/Python.h> // change this to your library path
 
-// #include <wiringPi.h>
+#include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -26,92 +26,93 @@ struct Measurement {
 // C function to read the sensor
 struct Measurement * read_dht_data_c(int DHT_PIN, struct Measurement *meas)
 {
-	// uint8_t laststate	= HIGH;
-	// uint8_t counter		= 0;
-	// uint8_t j			= 0, i;
+	uint8_t laststate	= HIGH;
+	uint8_t counter		= 0;
+	uint8_t j			= 0, i;
  
-	// data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+	data[0] = data[1] = data[2] = data[3] = data[4] = 0;
  
-	// /* pull pin down for 18 milliseconds */
-	// pinMode( DHT_PIN, OUTPUT );
-	// digitalWrite( DHT_PIN, LOW );
-	// delay( 18 );
+	/* pull pin down for 18 milliseconds */
+	pinMode( DHT_PIN, OUTPUT );
+	digitalWrite( DHT_PIN, LOW );
+	delay( 18 );
  
-	// /* prepare to read the pin */
-	// pinMode( DHT_PIN, INPUT );
+	/* prepare to read the pin */
+	pinMode( DHT_PIN, INPUT );
  
-	// /* detect change and read data */
-	// for ( i = 0; i < MAX_TIMINGS; i++ )
-	// {
-	// 	counter = 0;
-	// 	while ( digitalRead( DHT_PIN ) == laststate )
-	// 	{
-	// 		counter++;
-	// 		delayMicroseconds( 1 );
-	// 		if ( counter == 255 )
-	// 		{
-	// 			break;
-	// 		}
-	// 	}
-	// 	laststate = digitalRead( DHT_PIN );
+	/* detect change and read data */
+	for ( i = 0; i < MAX_TIMINGS; i++ )
+	{
+		counter = 0;
+		while ( digitalRead( DHT_PIN ) == laststate )
+		{
+			counter++;
+			delayMicroseconds( 1 );
+			if ( counter == 255 )
+			{
+				break;
+			}
+		}
+		laststate = digitalRead( DHT_PIN );
  
-	// 	if ( counter == 255 )
-	// 		break;
+		if ( counter == 255 )
+			break;
  
-	// 	/* ignore first 3 transitions */
-	// 	if ( (i >= 4) && (i % 2 == 0) )
-	// 	{
-	// 		/* shove each bit into the storage bytes */
-	// 		data[j / 8] <<= 1;
-	// 		if ( counter > 16 )
-	// 			data[j / 8] |= 1;
-	// 		j++;
-	// 	}
-	// }
+		/* ignore first 3 transitions */
+		if ( (i >= 4) && (i % 2 == 0) )
+		{
+			/* shove each bit into the storage bytes */
+			data[j / 8] <<= 1;
+			if ( counter > 16 )
+				data[j / 8] |= 1;
+			j++;
+		}
+	}
  
-	// /*
-	//  * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
-	//  * print it out if data is good
-	//  */
-	// if ( (j >= 40) &&
-	//      (data[4] == ( (data[0] + data[1] + data[2] + data[3]) & 0xFF) ) )
-	// {
-	// 	float h = (float)((data[0] << 8) + data[1]) / 10;
-	// 	if ( h > 100 )
-	// 	{
-	// 		h = data[0];	// for DHT11
-	// 	}
-	// 	float c = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
-	// 	if ( c > 125 )
-	// 	{
-	// 		c = data[2];	// for DHT22
-	// 	}
-	// 	if ( data[2] & 0x80 )
-	// 	{
-	// 		c = -c;
-	// 	}
-	// 	float f = c * 1.8f + 32;
-	//     (*meas).temp = c;
-    // 	(*meas).hum = h;
-    //     return meas;
-	// }else  {
-	//     return NULL;
-	// }
-
-    int r = rand() % 20;
-    
-    if(r%3 != 0){
-        float c = 20.3;
-        float h = 2.0;
-
+	/*
+	 * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+	 * print it out if data is good
+	 */
+	if ( (j >= 40) &&
+	     (data[4] == ( (data[0] + data[1] + data[2] + data[3]) & 0xFF) ) )
+	{
+		float h = (float)((data[0] << 8) + data[1]) / 10;
+		if ( h > 100 )
+		{
+			h = data[0];	// for DHT11
+		}
+		float c = (float)(((data[2] & 0x7F) << 8) + data[3]) / 10;
+		if ( c > 125 )
+		{
+			c = data[2];	// for DHT22
+		}
+		if ( data[2] & 0x80 )
+		{
+			c = -c;
+		}
+		float f = c * 1.8f + 32;
 	    (*meas).temp = c;
     	(*meas).hum = h;
-        return meas;	
+        return meas;
+	}else  {
+	    return NULL;
 	}
-    else {
-		// printf( "Data not good, skip\n" );
-        return NULL;
-	}
+
+	// Test code for my PC
+    // int r = rand() % 20;
+    
+    // if(r%3 != 0){
+    //     float c = 20.3;
+    //     float h = 2.0;
+
+	//     (*meas).temp = c;
+    // 	(*meas).hum = h;
+    //     return meas;	
+	// }
+    // else {
+	// 	// printf( "Data not good, skip\n" );
+    //     return NULL;
+	// }
 }
 
 // Python wrapper function to read the sensor
@@ -153,25 +154,3 @@ PyMODINIT_FUNC initdht11(void){
     Py_INCREF(dht11modError); // increase reference (since we own this ref)
     PyModule_AddObject(m, "error", dht11modError); // register to python interpreter
 }
-
- 
-// int main( void )
-// {
-// 	printf( "Raspberry Pi DHT11/DHT22 temperature/humidity test\n" );
- 
-// 	if ( wiringPiSetup() == -1 ) // ha med detta som ett exception?
-// 		exit( 1 );
- 
-// 	while ( 1 )
-// 	{
-// 		read_dht_data();
-// 		delay( 2000 ); /* wait 2 seconds before next read */
-// 	}
- 
-// 	return(0);
-// }
-
-
-// TODO:
-// kolla in libraries-argumentet
-// testa på tinkerboarden
